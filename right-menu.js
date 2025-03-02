de320c6409f7fbe6b55c15086fa9250ca3445c59
fatihozdemir23 <fatihozdemir571@gmail.com>
2025-03-02 18:07:26 +0300

    index değişti

--- right-menu.js
+++
@@ -1,7 +0,0 @@
- // Menü açma/kapama
-
- const rightMenu = document.getElementById('rightMenu');
- const openRightMenuBtn = document.getElementById('openRightMenu');
- openRightMenuBtn.addEventListener('click', function() {
-   rightMenu.classList.toggle('open');
- });
